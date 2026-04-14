dee8ce2124be563a8a8cf3060e284e8c9168b559
zeyad aldarandaly <zeyad.darandaly@gmail.com>
2026-04-14 20:10:05 +0200

    conn

--- backend-laravel/app/Services/AIService.php
+++ backend-laravel/app/Services/AIService.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Http;
 
 class AIService
 {
-    // public function generateDiagram($text, $type = 'auto')
-    // {
-    //     $response = Http::post('http://127.0.0.1:8000/generate-diagram', [
-    //         'text' => $text,
-    //         'type' => $type,
-    //     ]);
-
-    //     return $response->json();
-    // }
     public function generateDiagram($text, $type = 'auto')
     {
-        // MOCK RESPONSE
-        return [
-            "diagram_code" => "classDiagram\nUser --> Project\nProject --> Diagram",
-            "type" => $type
-        ];
+        $response = Http::post('http://127.0.0.1:8001/generate', [
+            'text' => $text,
+            'type' => $type,
+        ]);
+
+        dd($response->json());
     }
+    // public function generateDiagram($text, $type = 'auto')
+    // {
+    //     // MOCK RESPONSE
+    //     return [
+    //         "diagram_code" => "classDiagram\nUser --> Project\nProject --> Diagram",
+    //         "type" => $type
+    //     ];
+    // }
 }
--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// test
+Route::get('/test', function () {
+    return Http::post('http://127.0.0.1:8001/generate', [
+        'text' => 'test',
+        'type' => 'class'
+    ])->json();
 });
