e172fd017da8baef7144811643656275472e4057
zeyad aldarandaly <zeyad.darandaly@gmail.com>
2026-05-05 17:37:06 +0300

    update service

--- backend-laravel/app/Services/AIService.php
+++ backend-laravel/app/Services/AIService.php
@@ -6,21 +6,23 @@ use Illuminate\Support\Facades\Http;
 
 class AIService
 {
-    // public function generateDiagram($text, $type = 'auto')
-    // {
-    //     $response = Http::post('http://127.0.0.1:8001/generator', [
-    //         'text' => $text,
-    //         'type' => $type,
-    //     ]);
-
-    //     dd($response->json());
-    // }
     public function generateDiagram($text, $type = 'auto')
     {
-        // MOCK RESPONSE
-        return [
-            "diagram_code" => "classDiagram\nUser --> Project\nProject --> Diagram",
-            "type" => $type
-        ];
+        $response = Http::post('http://127.0.0.1:8001/generator', [
+            'text' => $text,
+            'type' => $type,
+        ]);
+
+        dd($response->json());
     }
+    
+    // TEST
+    // public function generateDiagram($text, $type = 'auto')
+    // {
+    //     // MOCK RESPONSE
+    //     return [
+    //         "diagram_code" => "classDiagram\nUser --> Project\nProject --> Diagram",
+    //         "type" => $type
+    //     ];
+    // }
 }
